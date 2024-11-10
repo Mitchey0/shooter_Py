806769b4b8768899881aba739e8a1a4fbea9655d
miche <ortizmich87@gmail.com>
2024-11-10 11:53:11 -0600

    Updating 'main.py' with notes for importing images and basic animation code(Left to right)

--- src/main.py
+++ src/main.py
@@ -6,11 +6,22 @@ display_surface = pygame.display.set_mode((window_width, window_height))
 pygame.display.set_caption('Shooter Shooter')
 running = True
 
+# Surface
+surf = pygame.surface((100, 200))
+surf.fill('orange')
+x = 100
+
+#Imported images
+# player_surf = pygame.image.load(join('images', 'filename.png')).convert_alpha()
+
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
     # ARTSSS
     display_surface.fill('seagreen1')
+    # display_surface.blit((width(Increase = right), height(increase = down)))
+    # x += 0.1
+    display_surface.blit(surf, (x, 150))
     pygame.display.update()
 pygame.quit()
