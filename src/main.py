a5cfbab3f4c1ba65a5e1edfb218931cd117e0e9d
miche <ortizmich87@gmail.com>
2024-11-10 10:32:18 -0600

    Updating 'main.py' for setting up basic display

--- src/main.py
+++ src/main.py
@@ -1,2 +1,16 @@
 import pygame
 
+window_width, window_height = 1280, 720
+pygame.init()
+display_surface = pygame.display.set_mode((window_width, window_height))
+pygame.display.set_caption('Shooter Shooter')
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    # ARTSSS
+    display_surface.fill('seagreen1')
+    pygame.display.update()
+pygame.quit()
