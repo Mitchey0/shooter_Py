7d714e6cb959da1513498bd5bd475b14908b0db5
miche <ortizmich87@gmail.com>
2024-11-13 02:26:12 -0600

    Clean and organize 'main.py'

--- src/main.py
+++ src/main.py
@@ -1,27 +1,28 @@
 import pygame
 
-window_width, window_height = 1280, 720
-pygame.init()
-display_surface = pygame.display.set_mode((window_width, window_height))
-pygame.display.set_caption('Shooter Shooter')
-clock = pygame.time.Clock()
-running = True
+class Game:
+    def __init__(self):   
+        window_width, window_height = 1280, 720
+        pygame.init()
+        self.display_surface = pygame.display.set_mode((window_width, window_height))
+        pygame.display.set_caption('Shooter Shooter')
+        clock = pygame.time.Clock()
+        running = True
 
-# Surface
-x = 650
-player.surf = Player()
-#Imported images
-# player_surf = pygame.image.load(join('images', 'filename.png')).convert_alpha()
+def run(self):
+    while running:
+        dt = self.clock.tick() / 1000
 
-while running:
-    dt = clock.tick() / 1000
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        # ARTSSS
+        self.display_surface.fill('seagreen1')
+        # display_surface.blit((width(Increase = right), height(increase = down)))
+        self.display_surface.blit(player.surf, (x, 550))
+        pygame.display.update()
+    pygame.quit()
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-    # ARTSSS
-    display_surface.fill('seagreen1')
-    # display_surface.blit((width(Increase = right), height(increase = down)))
-    display_surface.blit(player.surf, (x, 550))
-    pygame.display.update()
-pygame.quit()
+if __name__ == '__main__':
+    game = Game()
+    game.run()
