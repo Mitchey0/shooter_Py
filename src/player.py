1ffe929365d3341eff0912f785dee04ae6e745ae
miche <ortizmich87@gmail.com>
2024-11-13 01:41:27 -0600

    Quick note to add in 'player.py'

--- src/player.py
+++ src/player.py
@@ -1,5 +1,8 @@
 from main import *
 
+# Note: Just to be easier on me, unless I can find a way
+# to use the created surface, I can create quick sprites
+# and move forward for play testing!
 
 class Player(pygame.sprite.Sprite):
     def __init__(self, pos):
