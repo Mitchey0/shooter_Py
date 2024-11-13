b25a685f47a6445e72b900958d456d93eeb4a890
miche <ortizmich87@gmail.com>
2024-11-13 02:36:08 -0600

    Fixed indent issue in 'main.py' and uploaded png. into new 'image' folder.

--- src/main.py
+++ src/main.py
@@ -9,19 +9,19 @@ class Game:
         clock = pygame.time.Clock()
         running = True
 
-def run(self):
-    while running:
-        dt = self.clock.tick() / 1000
+    def run(self):
+        while running:
+            dt = self.clock.tick() / 1000
 
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-        # ARTSSS
-        self.display_surface.fill('seagreen1')
-        # display_surface.blit((width(Increase = right), height(increase = down)))
-        self.display_surface.blit(player.surf, (x, 550))
-        pygame.display.update()
-    pygame.quit()
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+            # ARTSSS
+            self.display_surface.fill('seagreen1')
+            # display_surface.blit((width(Increase = right), height(increase = down)))
+            self.display_surface.blit(player.surf, (x, 550))
+            pygame.display.update()
+        pygame.quit()
 
 if __name__ == '__main__':
     game = Game()
