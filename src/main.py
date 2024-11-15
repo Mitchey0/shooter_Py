c669c4d7c42670f767b42072221e3a859f40d8f9
miche <ortizmich87@gmail.com>
2024-11-15 13:30:17 -0600

    Removed extra '.py' files and evaluating a new idea

--- src/main.py
+++ src/main.py
@@ -1,34 +1,24 @@
-from setting import *
-from player import Player
+import pygame
+import os
 
-class Game:
-    def __init__(self):   
-        pygame.init()
-        self.display_surface = pygame.display.set_mode((window_width, window_height))
-        pygame.display.set_caption('Shooter Shooter')
-        self.clock = pygame.time.Clock()
-        self.running = True
+window_width, window_height = 720, 720
 
-        self.all_sprites = pygame.sprite.Group()
-        self.player = Player((500, 300), self.all_sprites)
+pygame.init()
 
-    def run(self):
-        while running:
-            dt = self.clock.tick() / 1000
+display_surface = pygame.display.set_mode((window_width, window_height))
+pygame.display.set_caption('Shooter Shooter')
+clock = pygame.time.Clock()
+running = True
 
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    running = False
-            
-            self.all_sprites.update(dt)
-        
-            self.display_surface.fill('seagreen1')
-            self.all_sprites.draw(self.display_surface)
-            # display_surface.blit((width(Increase = right), height(increase = down)))
-            # self.display_surface.blit(player.surf, (x, 550))
-            pygame.display.update()
-        pygame.quit()
+while running:
+    dt = clock.tick()
 
-if __name__ == '__main__':
-    game = Game()
-    game.run()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    display_surface.fill('seagreen1')
+    # display_surface.blit((width(Increase = right), height(increase = down)))
+    # self.display_surface.blit(player.surf, (x, 550))
+    pygame.display.update()
+pygame.quit()
--- src/setting.py
+++
@@ -1,5 +0,0 @@
-import pygame
-from os.path import join
-from os import walk
-
-window_width, window_height = 1280, 720
