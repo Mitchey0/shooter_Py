ae6a5d924a78d859cdcfe97087705d2ea1e1458e
miche <ortizmich87@gmail.com>
2024-11-16 11:56:48 -0600

    Rearranged code for window display and added settings for bullet and enemies. Added bullet class.

--- src/main.py
+++ src/main.py
@@ -2,30 +2,17 @@ import pygame
 import random
 
 pygame.init()
-
 window_width, window_height = 800, 600
 display_surface = pygame.display.set_mode((window_width, window_height))
 pygame.display.set_caption('Shooter Shooter')
-clock = pygame.time.Clock()
-running = True
 
-while running:
-    dt = clock.tick()
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-
-    display_surface.fill('seagreen1')
-    # display_surface.blit((width(Increase = right), height(increase = down)))
-    # self.display_surface.blit(player.surf, (x, 550))
-    pygame.display.update()
-
-pygame.quit()
-
+# Settings
 player_size = 50
 player_speed = 10
-
+bullet_size = 5
+bullet_speed = 15
+enemy_size = 50
+enemy_size = 4
 
 class Player():
     def __init__(self):
@@ -34,4 +21,41 @@ class Player():
     def move(self, dx, dy):
         self.rect.x += dx * player_speed
         self.rect.y += dx * player_speed
-        self.rect.clamp_ip(display_surface.get_rect())
+        Player.rect.clamp_ip(display_surface.get_rect()) # Code for keeping within screen was here
+
+class Bullet:
+    def __init__(self, x, y):
+        self.rect = pygame.Rect(x, y, bullet_size, bullet_size)
+    
+    def move(self):
+        self.rect.y -= bullet_size
+
+def main():
+    clock = pygame.time.Clock()
+    player = Player()
+    bullet = []
+    enemies = []
+    score = 0
+    running = True
+
+    while running:
+        dt = clock.tick()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            
+        keys = pygame.key.get_pressed()
+        dx = keys[pygame.K_d] - keys[pygame.K_a]
+        dy = keys[pygame.K_s] - keys[pygame.K_w]
+        player.move(dx, dy)
+
+        display_surface.fill('seagreen1')
+        # display_surface.blit((width(Increase = right), height(increase = down)))
+        # self.display_surface.blit(player.surf, (x, 550))
+        pygame.display.update()
+
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
