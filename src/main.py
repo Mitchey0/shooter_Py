be2f38e0508bfc45d3c89070e5818eb4219267d1
miche <ortizmich87@gmail.com>
2024-11-16 11:20:29 -0600

    Creating player class and adding player and mocement.

--- src/main.py
+++ src/main.py
@@ -1,10 +1,9 @@
 import pygame
-import os
-
-window_width, window_height = 720, 720
+import random
 
 pygame.init()
 
+window_width, window_height = 800, 600
 display_surface = pygame.display.set_mode((window_width, window_height))
 pygame.display.set_caption('Shooter Shooter')
 clock = pygame.time.Clock()
@@ -21,4 +20,18 @@ while running:
     # display_surface.blit((width(Increase = right), height(increase = down)))
     # self.display_surface.blit(player.surf, (x, 550))
     pygame.display.update()
+
 pygame.quit()
+
+player_size = 50
+player_speed = 10
+
+
+class Player():
+    def __init__(self):
+        self.rect = pygame.Rect(window_width // 2, window_height // 2, player_size, player_size)
+
+    def move(self, dx, dy):
+        self.rect.x += dx * player_speed
+        self.rect.y += dx * player_speed
+        self.rect.clamp_ip(display_surface.get_rect())
